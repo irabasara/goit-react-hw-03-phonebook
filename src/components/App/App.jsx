a193020e4cf580b972f3f9991f4079baa835a8c3
irabasara <125157770+irabasara@users.noreply.github.com>
2023-07-01 14:19:22 +0300

    add contacts in localestorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,11 @@ import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import css from './app.module.css';
 
+const LS_KEY_CONTACTS = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -53,6 +50,22 @@ export class App extends Component {
       contact.name.toLowerCase().includes(normilizeFilter)
     );
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        LS_KEY_CONTACTS,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(LS_KEY_CONTACTS));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
 
   render() {
     const filteredContact = this.getFilteredContact();
